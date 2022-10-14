e4f6c29669a6d53735fc2f870bb565aed11f0a1f
mide-etan <aworetanolamide@gmail.com>
2022-10-14 00:30:12 +0000

    size files

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,11 +3,12 @@
 /**
  * main - print the size of various types
  *
- * Description: print the size of various types on the computer it is compiled
+ * Description: prints the size of various types on the computer it is compiled
  * and run on
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 	printf("Size of a char: %d byte(s)\n", (int) sizeof(char));
@@ -15,5 +16,6 @@ int main(void)
 	printf("size of a long int: %d byte(s)\n", (int) sizeof(long int));
 	printf("size of a long long int: %d byte(s)\n", (int) sizeof(long long int));
 	printf("size of a float: %d byte(s)\n", (int) sizeof(float));
+
 	return (0);
 }
