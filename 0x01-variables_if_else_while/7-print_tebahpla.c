1876144ad6ffa786c36a73fe6e6c9ecfdfdce182
mide-etan <aworetanolamide@gmail.com>
2022-10-14 18:55:52 -0700

    print commands

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char le;
+
+	for (le = 'z'; le >= 'a'; le--)
+		putchar(le);
+
+	putchar('\n');
+
+	return (0);
+}
