37e74a9392bda183ce5b0d1bcc9c9b7fe6546f36
mide-etan <aworetanolamide@gmail.com>
2022-10-31 09:10:22 -0700

    largest number

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,17 +12,18 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if (a > b && a > c)
+	if (a > b && b > c && a > c)
 	{
 		largest = a;
 	}
-	else if (b > a && b > c)
-	{
-		largest = b;
-	}
-	else if (c > a && c > b)
+	else if(b < c)
 	{
 		largest = c;
 	}
+	else
+	{
+		largest = c;
+	}
+
 	return (largest);
 }
