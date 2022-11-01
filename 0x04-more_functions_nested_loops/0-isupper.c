deb9fa58876e5d521c56ab39e7e206a2addde750
mide-etan <aworetanolamide@gmail.com>
2022-11-01 10:15:27 +0100

    checks if character is uppercase

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,14 +2,18 @@
 
 /**
  * _isupper - cheks if number is uppercase
- *
+ * @c: variable to be checked
  * Return: 1 if c is uppercase else 0
  */
 
 int _isupper(int c)
 {
-	if( c >= 'A' && c <= 'Z')
-		return 1;
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
 	else
-		return 0;
+	{
+		return (0);
+	}
 }
