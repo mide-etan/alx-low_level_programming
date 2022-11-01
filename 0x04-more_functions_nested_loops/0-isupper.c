3925ecb36643249a539f246357afbe01e67893df
mide-etan <aworetanolamide@gmail.com>
2022-11-01 10:51:13 +0100

    check if it is upper case

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,19 +1,15 @@
 #include "main.h"
 
 /**
- * _isupper - cheks if number is uppercase
- * @c: variable to be checked
- * Return: 1 if c is uppercase else 0
+ * _isupper - checks for uppercase
+ * @c: character to checked for case
+ *
+ * Return: 1 if uppercase, 0 if not uppercase
  */
 
 int _isupper(int c)
 {
 	if (c >= 'A' && c <= 'Z')
-	{
 		return (1);
-	}
-	else
-	{
-		return (0);
-	}
+	return (0);
 }
