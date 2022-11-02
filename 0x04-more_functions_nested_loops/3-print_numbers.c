3a132cf16b58b65c173b7c6fa836850ef133d829
mide-etan <aworetanolamide@gmail.com>
2022-11-02 23:42:18 +0100

    prints number 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - function prints numbers from 0 to 9
+ *
+ *@c: variable that prints numbers
+ *
+ *Return: Return 0
+ */
+
+void print_numbers(void c)
+{
+	for(int c = 0; c < 10; c++)
+	{
+		putchar(c);
+	}
+	return (0);
+}
